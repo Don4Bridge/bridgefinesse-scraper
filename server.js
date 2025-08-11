dedfe8794990fdecec4b9fc185026a339194a5ba
Don4Bridge <77544197+Don4Bridge@users.noreply.github.com>
2025-08-10 20:32:30 -0700

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.listen(PORT, () => {
 app.get('/table', async (req, res) => {
   try {
     // Get today's date in yymmdd format
-    const today = new Date();
+    const today = (new Date()-2);
     const yy = String(today.getFullYear()).slice(2);
     const mm = String(today.getMonth() + 1).padStart(2, '0');
     const dd = String(today.getDate()).padStart(2, '0');
