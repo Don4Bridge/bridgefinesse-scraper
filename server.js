9b3fde7c8c3654f94f5e48c99cdb493b06bbb454
Don4Bridge <77544197+Don4Bridge@users.noreply.github.com>
2025-08-11 21:31:20 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,7 @@ const express = require('express');
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
-const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 const cheerio = require('cheerio');
-
-puppeteer.use(StealthPlugin());
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
